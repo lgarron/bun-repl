c703a1a5a25065ba1c244a97953eff87bc8ac6ce
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2023-03-07 08:40:18 -0300

    there was an attempt

--- src/replmanager.ts
+++ src/replmanager.ts
@@ -1,9 +1,32 @@
-import $ from './colors';
-import fs from 'fs';
-import os from 'os';
-import readline from 'rustybun';
+//import $ from './colors';
+//import fs from 'fs';
+//import os from 'os';
+import readline from 'readline/promises';
 
-export default class REPLManager extends readline {
+/** 
+ * Bun node:readline polyfill is still very broken it seems
+ * so this branch will have to wait for now
+ * @see https://github.com/oven-sh/bun/issues/2333
+ * @see https://github.com/oven-sh/bun/issues/2035
+ */
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: true,
+    tabSize: 4,
+    prompt: '> ',
+    historySize: 10,
+    history: ['test history entry 1', 'test history entry 2'],
+    //completer(line: string) {
+    //    const completions = ['hello', 'world'];
+    //    const hits = completions.filter(c => c.startsWith(line));
+    //    return [hits.length ? hits : completions, line];
+    //}
+});
+rl;
+
+/*
+export default class REPLManager extends null {
     constructor(prompt: string = '> ', historyPath?: string) {
         super();
         this.prompt = process.env.BUN_REPL_PROMPT ?? prompt;
@@ -45,4 +68,4 @@ export default class REPLManager extends readline {
     prompt: string;
     readonly #historyfd: number;
     readonly #historypath: string;
-}
+}*/
